0f6439170c75a4d0bef80e65cfbdab4a7feadcd6
kingofpiano <mrkingofjazzpiano@gmail.com>
2022-06-17 01:38:24 +0300

    add link to kata

--- 8_kyu/xor.c
+++ 8_kyu/xor.c
@@ -1,3 +1,4 @@
+// https://www.codewars.com/kata/56fa3c5ce4d45d2a52001b3c
 #include <stdio.h>
 #include <stdbool.h>
 
@@ -6,13 +7,14 @@ static bool xor(bool a, bool b){
     return (bool)(a != b);
 }
 
+
 int main(void){
     printf("xor(0, 0)=%d\n", (int)xor(0, 0));
     printf("xor(0, 1)=%d\n", (int)xor(0, 1));
     printf("xor(1, 0)=%d\n", (int)xor(1, 0));
     printf("xor(1, 1)=%d\n", (int)xor(1, 1));
 
-    // printf("xor(5, 7)=%d\n", (int)xor(5, 7));
+    printf("xor(5, 7)=%d\n", (int)xor(5, 7));
 
     printf("\nFinish\n");
 }
