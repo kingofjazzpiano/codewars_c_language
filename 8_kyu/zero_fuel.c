c15226dd73fdbe925f25ce3c3694e0bd9843bfd0
kingofpiano <mrkingofjazzpiano@gmail.com>
2022-06-17 03:35:39 +0300

    zero_fuel.c

---
+++ 8_kyu/zero_fuel.c
@@ -0,0 +1,19 @@
+// https://www.codewars.com/kata/5861d28f124b35723e00005e/train/c
+#include <stdio.h>
+#include <stdbool.h>
+
+
+static bool zero_fuel(double distance_to_pump, double mpg, double fuel_left)
+{
+    if (fuel_left * mpg < distance_to_pump){
+        return false;
+    }
+    return true;
+}
+
+
+int main(void){
+    printf("Is it possible? %d\n", zero_fuel(50.0, 25.0, 2.0));
+
+    printf("\nFinish\n");
+}
